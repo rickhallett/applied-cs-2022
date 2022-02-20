1903d060e5a4eaf0091ab0fe8182e5221e8dbba8
Rick Hallett <rick.hallett@brandwatch.com>
2022-02-20 15:17:06 +0000

    init cra with spectre.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import 'spectre.css'
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            <header className="navbar">
+                <section className="navbar-section">
+                    <a href="/docs" className="btn btn-link">
+                        Docs
+                    </a>
+                    <a href="/examples" className="btn btn-link">
+                        Examples
+                    </a>
+                </section>
+                <section className="navbar-center">
+                    {/* <!-- centered logo or brand --> */}
+                </section>
+                <section className="navbar-section">
+                    <a href="/twitter" className="btn btn-link">
+                        Twitter
+                    </a>
+                    <a href="/github" className="btn btn-link">
+                        GitHub
+                    </a>
+                </section>
+            </header>
+            <main>
+                <div className="container">
+                    <div className="columns oneline">
+                        <div className="column col-6 bg-gray">col-1</div>
+                        <div
+                            className="divider-vert text-center"
+                            data-content="|"
+                        ></div>
+                        <div className="column col-6 bg-gray">col-2</div>
+                    </div>
+                </div>
+            </main>
+        </>
+    )
 }
 
-export default App;
+export default App
